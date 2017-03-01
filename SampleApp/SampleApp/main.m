b1ff397a5f64a25f01fdc48eeb888292d4074f50
Venky <narasimhan@samsung.com>
2017-03-01 14:14:39 -0600

    Initial dummy app

---
+++ SampleApp/SampleApp/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Venky on 2/27/17.
+//  Copyright © 2017 Samsung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "DgApp.h"
+
+int main(int argc, const char * argv[]) {
+   
+   DgApp *pDgApp = [[DgApp alloc] init];
+   
+   NSApplication *pApp = [NSApplication sharedApplication];
+   [pApp setDelegate:pDgApp];
+   [pApp performSelectorOnMainThread:@selector(run) withObject:nil waitUntilDone:YES];
+   [pApp release];
+   [pDgApp release];
+   
+   return EXIT_SUCCESS;
+}
